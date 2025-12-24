3fa376019c10bbb6dec60ef93acf475b19f2e194
Germano Ricardi <germanoricardi7@gmail.com>
2025-12-24 15:16:32 -0400

    feat: enhance dashboard layout with AppBar and sign-out functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,35 @@
 'use client';
 
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import { signOut, useSession } from "next-auth/react";
-
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import Image from "next/image";
 export default function DashboardPage() {
   const { data: session, status } = useSession();
 
+  const handleSignOut = () => signOut({
+    redirect: true,
+    callbackUrl: '/'
+  })
+
   return (
-    <div>
-      <h1>Bem-vindo ao Dashboard!</h1>
-      <h1>Dashboard</h1>
-      <p>Bem-vindo, {session?.user?.name || session?.user?.email}!</p>
-      <p>Seu accessToken: {session?.accessToken}</p>
-      <p>
-        <button onClick={() => signOut({
-          redirect: true,
-          callbackUrl: '/'
-        })}>Sair</button>
-      </p>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Image src="/images/almoxapp.svg" alt="AlmoxApp" width={120} height={50} style={{ margin: '0 auto' }} />
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              edge="end"
+              onClick={handleSignOut}
+              color="inherit"
+            >
+              <MeetingRoomIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
 );
   
 }
